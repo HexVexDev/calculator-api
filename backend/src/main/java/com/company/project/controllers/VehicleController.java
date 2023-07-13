8b569ee4b210f256d73acca5295d1e476930095c
HexVexDev <hexvexdeveloper@gmail.com>
2023-07-13 14:18:51 -0700

    changed pom xml

--- backend/src/main/java/com/company/project/controllers/VehicleController.java
+++ backend/src/main/java/com/company/project/controllers/VehicleController.java
@@ -1,40 +1,30 @@
-//package com.company.project.controllers;
-//import com.company.project.services.VehicleService;
-//import com.fathzer.soft.javaluator.DoubleEvaluator;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class VehicleController {
-//
-//    @Autowired
-//    VehicleService service;
-//
-//  @CrossOrigin(origins = "http://localhost:3000")
-//  @PostMapping
-//
-//
-//  public String calculate(@RequestBody RequestBod requestbody) {
-//        String expression = requestbody.getInput();
-//        DoubleEvaluator eval = new DoubleEvaluator();
-//
-//        Double result = eval.evaluate(expression);
-//        String output =result.toString();
-//        return output;
-//  }
-//  public static class RequestBod{
-//
-//      private String input;
-//
-//      public String getInput() {
-//        return input;
-//        }
-//
-//      public void setInput(String input) {
-//        this.input = input;
-//      }
-//    }
-//}
+package com.company.project.controllers;
+
+import com.company.project.entities.Vehicle;
+import com.company.project.services.VehicleService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+    @Autowired
+    VehicleService service;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping()
+    public ArrayList<Vehicle> getAllVehicles(){
+        return service.getAllVehicles();
+    }
+
+    @PostMapping()
+    public Vehicle saveVehicle(@RequestBody Vehicle vehicle){
+        return this.service.saveVehicle(vehicle);
+    }
+}
+
 
